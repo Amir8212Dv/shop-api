c1ae493d7ac5fafd2a86d2906371b7054f87d41d
Amir Mohammad Razmi <88226929+Amir8212Dv@users.noreply.github.com>
2022-08-08 01:02:21 +0430

    add joi-phone-number package to application

--- src/app/validators/user/auth.js
+++ src/app/validators/user/auth.js
@@ -1,30 +1,29 @@
-import joi from 'joi'
+import joiValidator from 'joi'
+import validatePhoneNumber  from 'joi-phone-number'
+
+const joi = joiValidator.extend(validatePhoneNumber)
 
 export const signupUserValidationSchema = joi.object({
     first_name : joi.string().required(),
     last_name : joi.string().required(),
     email : joi.string().lowercase().email().error(new Error('please inter a valid email')),
     password : joi.string().min(8).required(),
-    mobile : joi.string().required()
-    .pattern(/(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/)
+    mobile : joi.string().required().phoneNumber()
     .error(new Error('please enter a valid phone number'))
 })
 
 export const loginWithPasswordValidationSchema = joi.object({
     email : joi.string().lowercase().email().error(new Error('please inter a valid email')),
     password : joi.string().required(),
-    mobile : joi.string()
-    .pattern(/(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/)
+    mobile : joi.string().phoneNumber()
     .error(new Error('please enter a valid phone number'))
 })
 export const loginWithOtpValidationSchema = joi.object({
-    mobile : joi.string().required()
-    .pattern(/(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/)
+    mobile : joi.string().required().phoneNumber()
     .error(new Error('please enter a valid phone number'))
 })
 export const checkOtpValidationSchema = joi.object({
     code : joi.string(),
-    mobile : joi.string().required()
-    .pattern(/(0|\+98)?([ ]|-|[()]){0,2}9[1|2|3|4]([ ]|-|[()]){0,2}(?:[0-9]([ ]|-|[()]){0,2}){8}/)
+    mobile : joi.string().required().phoneNumber()
     .error(new Error('please enter a valid phone number'))
 })
